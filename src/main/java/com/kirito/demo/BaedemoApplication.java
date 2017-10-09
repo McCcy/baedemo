a84eadfcdb48870733076123ba1f5ff2cc1465de
ccy <ccy@163.com>
2017-10-09 15:19:59 +0800

    Initial commit

---
+++ src/main/java/com/kirito/demo/BaedemoApplication.java
@@ -0,0 +1,12 @@
+package com.kirito.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaedemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/kirito/demo/controller/MyController.java
@@ -0,0 +1,20 @@
+package com.kirito.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ccy on 2017/9/21.
+ */
+@Controller
+public class MyController {
+    @RequestMapping("/index")
+    String index() {
+        return "index";
+    }
+
+    @RequestMapping("/")
+    String hello() {
+        return "hello";
+    }
+}
